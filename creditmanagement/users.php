ddb5437c770e7dc6df175534a93c9bfbd02c081d
shwetapatil425 <59535329+shwetapatil425@users.noreply.github.com>
2020-09-30 11:27:11 +0530

    Update users.php

--- creditmanagement/users.php
+++ creditmanagement/users.php
@@ -6,6 +6,7 @@
 table, th, td {
   border: 1px solid black;
   border-collapse: collapse;
+  padding:10px;
 }
 body{
  margin:0;
@@ -38,15 +39,31 @@ color: green;
 tr{
 color:black;
 }
-}
 
 </style>
 </head>
-<body><form>
-<center>
-<br><br><br><br><br>
-<h2>LIST OF USERS</h2>
-<table>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="home.html">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="users.php">Transaction History<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="list.php">View All Users</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="transfer.html">Transfer Money</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<br>
+<table style="width:100%">
 <tr>
 <th>SENDER</th>
 <th>RECEIVER</th>
@@ -73,11 +90,10 @@ echo "0 results";
 $conn->close();
 ?>
 </table>
-<br>
-<br>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
-<a href="home.html"><button type="button" class="btn btn-primary">BACK</button></a>
-</center>
-</form>
+
 </body>
 </html>
