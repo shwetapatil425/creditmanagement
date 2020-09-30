30eb0066fd9651e61d7cbe103f2a1aba7394fba3
shwetapatil425 <59535329+shwetapatil425@users.noreply.github.com>
2020-09-30 11:23:28 +0530

    Update list.php

--- creditmanagement/list.php
+++ creditmanagement/list.php
@@ -6,6 +6,7 @@
 table, th, td {
   border: 1px solid black;
   border-collapse: collapse;
+  padding:10px;
 }
 body{
  margin:0;
@@ -34,20 +35,41 @@ color: #d96459;
 }
 th{
 color:green;
+padding:15px;
 }
 tr{
 color:black;
+padding:15px;
 }
+h2{
+  color:gold;
 }
 
 </style>
 </head>
-<body><form>
-<center>
-<br><br><br><br><br>
-<h2>LIST OF USERS</h2>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="home.html">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="users.php">Transaction History<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="list.php">View All Users</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="transfer.html">Transfer Money</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<center><h2>LIST OF USERS</h2></center>
 <br>
-<table>
+<table style="width:100%">
 <tr>
 <th>USERNAME</th>
 <th>E-MAIL</th>
@@ -76,10 +98,11 @@ $conn->close();
 </table>
 <br>
 <br>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
-<a href="transfer.html"><button type="button" class="btn btn-primary">SELECT USER</button></a>
-<a href="home.html"><button type="button" class="btn btn-primary">BACK</button></a>
-</center>
-</form>
+
+
 </body>
 </html>
